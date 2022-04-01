239b4e1fb330a245ddda294cd01dca7141694484
Dana <danacarroll2000@gmail.com>
2022-04-01 12:51:39 -0400

    Updates final display, does not include fund length changes

--- js/app.js
+++ js/app.js
@@ -44,6 +44,11 @@ const dispLength = document.getElementById('monthsLasting');
 // Setting default fundlength and expenses
 fundLengthInput.value = 6;
 let fundLength = 6;
+let fundTotal = 0;
+// Setting section monthly totals
+let housingMonthlyTotal = 0;
+let transportMonthlyTotal = 0;
+let monthlyTotal = 0;
 
 // Set all input values to 0
 sectionsMonthly.forEach(section => {
@@ -58,6 +63,18 @@ dispMonthTotal.innerHTML = 0;
 dispFundTotal.innerHTML = 0;
 dispLength.innerHTML = 6;
 
+// When fund length changes
+fundLengthInput.addEventListener('change', () => {
+    updateFundLength();
+})
+
+function updateFundLength() {
+    let newLength = fundLengthInput.value;
+    newLength = parseInt(newLength, 10);
+    fundLength = newLength;
+    dispLength.innerHTML = fundLength;
+}
+
 // Grabbing all inputs from each section
 let housingInputs = [];
 let transportInputs = [];
@@ -68,13 +85,23 @@ housingInputList.forEach(input => {
 
     input.addEventListener('change', () => {
         let newInputs = [];
+        // Update all input values and push to sections input array
         housingInputList.forEach(input => {
             let newInputValue = input.value;
             let newInputNum = parseInt(newInputValue, 10);
             newInputs.push(newInputNum);
             housingInputs.splice(0, housingInputs.length, ...newInputs);
         });
+        console.log('--- HOUSING INPUTS ---');
         console.log(housingInputs);
+        // Update total monthly amount for section
+        let sum = [...housingInputs].reduce((a , b) => a + b, 0);
+        housingMonthly.innerHTML = sum;
+        housingMonthlyTotal = sum;
+        updateDisplay(housingMonthlyTotal, fundLength);
+
+        console.log(`Housing monthly total = ${housingMonthlyTotal}`);
+        console.log(`New overall monthly total = ${monthlyTotal}`);
     })
 });
 transportInputList.forEach(input => {
@@ -84,15 +111,35 @@ transportInputList.forEach(input => {
 
     input.addEventListener('change', () => {
         let newInputs = [];
+        // Update all input values and push to sections input array
         transportInputList.forEach(input => {
             let newInputValue = input.value;
             let newInputNum = parseInt(newInputValue, 10);
             newInputs.push(newInputNum);
             transportInputs.splice(0, transportInputs.length, ...newInputs);
         });
+        console.log('--- TRANSPORT INPUTS ---');
         console.log(transportInputs);
+        // Update total monthly amount for section
+        let sum = [...transportInputs].reduce((a , b) => a + b, 0);
+        transportMonthly.innerHTML = sum;
+        transportMonthlyTotal = sum;
+        updateDisplay(transportMonthlyTotal, fundLength);
+
+
+        console.log(`Transport monthly total = ${transportMonthlyTotal}`);
+        console.log(`New overall monthly total = ${monthlyTotal}`);
     })
 });
+
+function updateDisplay(sectionTotal, fundLength) {
+    monthlyTotal += sectionTotal;
+    fundTotal = monthlyTotal*fundLength;
+    dispMonthTotal.innerHTML = monthlyTotal;
+    dispFundTotal.innerHTML = fundTotal;
+    dispLength.innerHTML = fundLength;
+}
+
 
 
 // function calcFund(fund, a, b, c) {
